37bfb31454569d06d6ff40b608f631a5e4a38fce
grgp <g.a.pitoy@gmail.com>
2015-10-24 18:17:17 +0700

    good enough cards layout

--- _assets/js/newpost.js
+++ _assets/js/newpost.js
@@ -22,9 +22,24 @@ function newpost() {
 	$.each(newerPosts, function (i, user) {
         var postcard = 
         	(
-        	'<div class="mdl-cell mdl-card mkk mdl-shadow--2dp"><div class="mdl-card__title">' 
-        	+ user.username + '</div><div class="mdl-card__supporting-text">' 
-        	+ user.post + '|' + user.date + '</div><div class="mdl-card__actions"></div></div>'
+        	'<div class="mdl-cell mdl-shadow--2dp cardwidth">' +
+							'<div class="mdl-grid">' +
+								'<div class="mdl-cell mdl-cell--2-col profileimg">' +
+									'<img src="_assets/data/image/' + user.username.toLowerCase() + '.png" class="profileimg"/>' +
+								'</div>' +
+								'<div class="mdl-cell mdl-cell--8-col carddet">' +
+								  '<div class="mdl-card__title">' + user.username + '</div>' +
+								  '<div class="mdl-card__supporting-text">' + user.post +
+								  '</div>' +
+								  '<div class="mdl-card__actions mdl-card--border">' +
+								  	'<span class="postdate">posted at ' + user.date + '</span>' +
+								  	'<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect likes">' +
+	      							user.like + ' Likes' +
+	    							'</a>' +
+								  '</div><!-- button -->' +
+								'</div><!-- mdlcell 8 -->' +
+							'</div><!--mdl-grid-->' +
+						'</div>'
         	);
         $('#posts').prepend(postcard);
   }); //$.each(...)
